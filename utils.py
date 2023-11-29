5c7ae3a0639d74d2a79bd75117208b17bfae0f8f
amirmahdi-alavi <alavi_amirmahdi@yahoo.com>
2023-11-29 16:16:59 +0330

    added box size and border size to create_from_csv

--- utils.py
+++ utils.py
@@ -1,10 +1,11 @@
 from encode import EncodeQRCode
 import textwrap
 from PIL import Image, ImageDraw, ImageFont
+import numpy as np
 import os
 
 
-def create_image(width, height, bgColor, text, text_size=10, alligned_width=10):
+def create_image(width, height, bgColor, text, text_size=20, alligned_width=10):
     import arabic_reshaper
     from bidi.algorithm import get_display
     # text = text.encode('utf-8')
@@ -23,22 +24,21 @@ def create_image(width, height, bgColor, text, text_size=10, alligned_width=10):
     return img
 
 
-def create_from_csv(file_path):
+def create_from_csv(file_path, logo_size=100, box_size=100, border_size=20):
     import pandas as pd
-    df = pd.read_csv(file_path)
+    df = pd.read_csv(file_path, dtype={'Product Code': int, 'Serial': int})
     os.makedirs('QRCodes', exist_ok=True)
     for idx, row in df.iterrows():
-        data_dict = row.to_dict()
+        data_dict = row[['Serial', 'Product Code']].astype('str').to_dict()
         qr_instance = EncodeQRCode(
-            path_name=os.path.join('QRCodes', f'{data_dict["ProductCode"]}.jpg'),
-            data = data_dict,   
+            path_name=os.path.join('QRCodes', f'{data_dict["Product Code"]}.jpg'),
+            data = data_dict,
+            box_size=box_size,
+            border_size=border_size,
+            fill_color='black',
+            back_color='white'
         )
         qr_instance.create_qrcode()
-        logo = create_image(width=100, height=100, bgColor=(255, 255, 255), text=data_dict['SKUName'])
-        qr_instance.add_logo(logo, 100)
+        logo = create_image(width=100, height=100, bgColor=(255, 255, 255), text=row['SKUName'])
+        qr_instance.add_logo(logo, logo_size)
         qr_instance.save()
-    
-
-
-
-
