34e98802688005f793239a0af641c0aba441a7e1
amirmahdi-alavi <sam.alavi@hotmail.com>
2024-04-05 20:04:43 -0400

    Decode class

--- decode.py
+++ decode.py
@@ -1,9 +1,37 @@
 import cv2
 import json
+from pyzbar.pyzbar import decode
+from PIL import Image
+import numpy as np
 
 
-class DecodeQrCode:
-    def __init__(self, source):
-        self.source = source
-        self.qr_reader = cv2.QRCodeDetecto
+
+class Decode:
+    def __init__(self, image):
+        self.image = image
+
+
+    def detect(self):
+        if isinstance(self.image, np.ndarray) or isinstance(self.image, Image):
+            res = decode(self.image)
+        elif isinstance(self.image, str):
+            image = Image.open(self.image)
+            res = decode(image)
+        else:
+            raise TypeError(f'wrong image input with type ({type(self.image)}). You should input either str, np array, or PIL Image')
+        
+        result_dicts = [{
+            'data': qr.data.decode('utf-8'),
+            'boundingBox': {
+                'x': qr.rect.left,
+                'y': qr.rect.top,
+                'width': qr.rect.width,
+                'height': qr.rect.height
+            },
+            'orientation': qr.orientation
+        } for qr in res]
+
+        return result_dicts 
+
+
 
